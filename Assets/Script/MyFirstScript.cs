d0d228349c95925de8a6e8470357a158da188ea0
apw <anggitprayogo@gmail.com>
2017-09-25 09:47:43 +0700

    initial commit

---
+++ Assets/Script/MyFirstScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour {
+
+	public GameObject myObj;
+	public GameObject myBase;
+	public GameObject thePrefab;
+	Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		myObj = GetComponent<GameObject> ();
+		myBase = GetComponent<GameObject> ();
+
+		myTransform = GetComponent<Transform> ();
+	}
+	 	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("space")){
+			Instantiate (thePrefab);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){ 	
+		if(collision.gameObject.name == "Base"){
+			GameObject.Find ("Cube").SetActive (false);
+		}
+
+
+	}
+}
